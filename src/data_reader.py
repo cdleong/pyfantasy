520cc81e03e3b42fee1fc982f2d871a7deb747eb
cdleong <cdleong+github@gmail.com>
2018-08-29 20:18:57 -0400

    CDL: Initial "hello world" commit

---
+++ src/data_reader.py
@@ -0,0 +1,20 @@
+'''
+Created on Aug 29, 2018
+
+@author: cdleong
+'''
+
+class fantasy_data_reader(object):
+    '''
+    reads data
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print("hello world", self)
+        
+if __name__ == "__main__":
+    bob = fantasy_data_reader()
