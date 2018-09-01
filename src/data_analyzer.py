01eb9dbf339f8b33a74b3674703b2c7ca1661fa9
cdleong <cdleong+github@gmail.com>
2018-09-01 15:00:10 -0400

    CDL: Stub files, renaming, etc.

---
+++ src/data_analyzer.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 1, 2018
+
+@author: cdleong
+'''
+
+class PyFantasyDataAnalyzer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor        
+        '''
+        print(f"Initializing {self}")
+    
+
+def main():
+    # TODO: get the data using data_analyzer 
+    pfda = PyFantasyDataAnalyzer()
+    
+    
+if __name__ == '__main__':
+    main()
--- src/data_parser.py
+++ src/data_parser.py
@@ -4,17 +4,17 @@ Created on Aug 29, 2018
 @author: cdleong
 '''
 
-class fantasy_data_reader(object):
+class PyFantasyDataParser(object):
     '''
-    reads data
+    reads data!
     '''
-
 
     def __init__(self):
         '''
         Constructor
         '''
-        print("hello world", self)
+        print(f"Initializing {self}")
         
 if __name__ == "__main__":
-    bob = fantasy_data_reader()
+    bob = PyFantasyDataParser() 
+    
