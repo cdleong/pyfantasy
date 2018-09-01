84a0945333688614316d8c67ab0f475a754b8661
cdleong <cdleong+github@gmail.com>
2018-09-01 14:58:03 -0400

    CDL: refactored Yahoo interface to a class.

--- src/yahoo_interface.py
+++ src/yahoo_interface.py
@@ -3,125 +3,139 @@ Created on Aug 29, 2018
 
 @author: cdleong
 
-
+TODO: OO it to PyFantasyYahooInterface
 '''
 import YahooSports as ysp
 import math
 
-# CONSTANTS
-NFL_PLAYERS_PER_TEAM = 53
-NUMBER_OF_NFL_TEAMS = 32
-NUMBER_OF_DEFENSES = NUMBER_OF_NFL_TEAMS
-MAX_NFL_PLAYERS = (NFL_PLAYERS_PER_TEAM*NUMBER_OF_NFL_TEAMS) + NUMBER_OF_DEFENSES # TODO: fix this. There's actually about double this number somehow
-MAX_RETURNED_PER_QUERY = 25  # determined experimentally
-MAX_QUERY = math.ceil(MAX_NFL_PLAYERS/MAX_RETURNED_PER_QUERY)*2 
-POSSIBLE_POSITIONS = ["QB", "WR", "RB", "TE", "K" , "DEF"]
 
 
-def make_saved_session(auth_filename):
-    session = ysp.YahooConnection(auth_filename)
-    if not session.is_live_session():
-        url = session.auth_url()
-        print("Go to URL:")
-        print(url)
-        pin = input('Enter PIN from browser: ')
-        session.enter_pin(pin)
+class PyfantasyYahooSportsInterface(object):
+    # CLASS CONSTANTS
+    NFL_PLAYERS_PER_TEAM = 53
+    NUMBER_OF_NFL_TEAMS = 32
+    NUMBER_OF_DEFENSES = NUMBER_OF_NFL_TEAMS
+    
+    # experimentally determined that there's 2789 "players" in the Yahoo DB, actually
+    # TODO: fix calculation method
+    MAX_NFL_PLAYERS = (NFL_PLAYERS_PER_TEAM*NUMBER_OF_NFL_TEAMS) + NUMBER_OF_DEFENSES
+    MAX_RETURNED_PER_QUERY = 25  # determined experimentally
+    MAX_QUERY = math.ceil(MAX_NFL_PLAYERS/MAX_RETURNED_PER_QUERY)*2 
+    POSSIBLE_POSITIONS = ["QB", "WR", "RB", "TE", "K" , "DEF"]
+
+    
+    def __init__(self, auth_filename):
+        '''
+        Constructor        
+        '''
+        print(f"Initializing {self}")
+        self.connect(auth_filename)
+
+    def connect(self, auth_filename):
         
-def use_saved_session(auth_filename):
-    session = ysp.YahooConnection(auth_filename)
-    return session
-
-def query_yahoo(session, query):    
-    return session.get(query)
-
-def get_xml_from_yahoo_result(result):
-    
-    return result.clean_text
-
-
-def get_players_data(session, position=""):
-    xml_results = []
-    count = MAX_RETURNED_PER_QUERY
-    start = 0
-    count_string = "players count"
-    i = 0
-    more_players_to_retrieve = True
-    
-    
-    while i < MAX_QUERY and more_players_to_retrieve:
-        start = i * count
-        i += 1
+        # Try connecting with saved session
+        session = ysp.YahooConnection(auth_filename)
         
-        print(f"i: {i}, start:{start}")
+        # Oh no it's not live
+        if not session.is_live_session():
+            url = session.auth_url()
+            print("Go to URL:")
+            print(url)
+            pin = input('Enter PIN from browser: ')
+            session.enter_pin(pin)
         
-        # base query
-        query = "game/nfl/players;out=draft_analysis,percent_owned"
+        self.session = session    
+    
+    
+    def query_yahoo(self, query):    
+        return self.session.get(query)
+    
+    def get_xml_from_yahoo_result(self, result):        
+        return result.clean_text
+    
+    
+    def get_players_data(self, position=""):
+        xml_results = []
+        count = PyfantasyYahooSportsInterface.MAX_RETURNED_PER_QUERY
+        start = 0
+        count_string = "players count"
+        i = 0
+        more_players_to_retrieve = True
         
-        # Just one position?
-        if position:
-            print(f"adding position {position} to query")
-            query = query + ";position=" + position
-                
-        query = query+";count=" + str(count)             
-        query = query + ";start=" + str(start)
         
-        print(f"Final query: {query}")
+        while i < PyfantasyYahooSportsInterface.MAX_QUERY and more_players_to_retrieve:
+            start = i * count
+            i += 1
+            
+            print(f"i: {i}, start:{start}")
+            
+            # base query
+            query = "game/nfl/players;out=draft_analysis,percent_owned"
+            
+            # Just one position?
+            if position:
+                print(f"adding position {position} to query")
+                query = query + ";position=" + position
+                    
+            query = query+";count=" + str(count)             
+            query = query + ";start=" + str(start)
+            
+            print(f"Final query: {query}")
+            
+            result = self.query_yahoo(query)
+            xml = self.get_xml_from_yahoo_result(result)
+            xml_results.append(xml)
+            if count_string not in xml:
+                print("count is zero, breaking out of loop")
+                more_players_to_retrieve = False
+            else:
+                matched_lines = [line for line in xml.split('\n') if count_string in line]
+                print("COUNT: {}".format(matched_lines))
         
-        result = query_yahoo(session, query)
-        xml = get_xml_from_yahoo_result(result)
-        xml_results.append(xml)
-        if count_string not in xml:
-            print("count is zero, breaking out of loop")
-            more_players_to_retrieve = False
-        else:
-            matched_lines = [line for line in xml.split('\n') if count_string in line]
-            print("COUNT: {}".format(matched_lines))
+        return xml_results
     
-    return xml_results
-
-    
-def query_league(session, subquery="", league_number=None,):
-    if not league_number:
-        league_number = input("League number is: ")
-    query = "league/nfl.l."+str(league_number) +subquery # get league info    
-    
-    return query_yahoo(session, query)
         
-#     brett_favre_xml = session.get("game/223/players;player_keys=223.p.1025/draft_analysis")
-#     print(brett_favre_xml)
-#     print(brett_favre_xml.clean_text)
+    def query_league(self, subquery="", league_number=None,):
+        if not league_number:
+            league_number = input("League number is: ")
+        query = "league/nfl.l."+str(league_number) +subquery # get league info    
+        
+        return self.query_yahoo(query)
+            
+    #     brett_favre_xml = session.get("game/223/players;player_keys=223.p.1025/draft_analysis")
+    #     print(brett_favre_xml)
+    #     print(brett_favre_xml.clean_text)
+        
+        #http://fantasysports.yahooapis.com/fantasy/v2/game/nfl
+    #     nfl_games_xml = session.get("game/nfl")
+    #     print(nfl_games_xml.clean_text) 
+        
+    #     players_xml = session.get("game/nfl/players")
+    #     print(players_xml.clean_text)
+         
     
-    #http://fantasysports.yahooapis.com/fantasy/v2/game/nfl
-#     nfl_games_xml = session.get("game/nfl")
-#     print(nfl_games_xml.clean_text) 
-    
-#     players_xml = session.get("game/nfl/players")
-#     print(players_xml.clean_text)
-     
-
-def append_to_text_file(filename, text_to_append):
-    filename = filename + ".txt"
-    with open(filename, "a") as text_file:     
-        print(f"{text_to_append}", file=text_file)    
+    def clear_text_file_and_write(self, filename, contents_to_write):
+        filename = filename + ".txt"
+        
+        #clear it first
+        open(filename, 'w').close()
+        
+        #  add the text
+        with open(filename, "a") as text_file:     
+            print(f"{contents_to_write}", file=text_file)    
     
     
 if __name__ == "__main__":
 
     auth_filename="auth_keys.txt"
-    session = make_saved_session(auth_filename)
-    session = use_saved_session(auth_filename)
+    pyfsi = PyfantasyYahooSportsInterface(auth_filename)
         
-    query = "game/nfl/players" # get all the players
-    query = "game/nfl/players/draft_analysis" # get all the players, with draft analysis
-    query = "game/nfl/players/percent_owned" # get all the players, with percent_owned
-    query = "game/nfl/players;out=draft_analysis,percent_owned" # get all the players, with percent_owned
-    query = "game/nfl/players;out=draft_analysis,percent_owned;position=QB;count=25"
     
     # experimentally determined that there's 2789 "players" in the Yahoo DB. 
-    xml_results = get_players_data(session)
+    xml_results = pyfsi.get_players_data()
     filename = "all_players" + ".txt" 
     for result in xml_results:
-        append_to_text_file(filename, result)
+        pyfsi.clear_text_file_and_write(filename, result)
     
 #     
 #     # get a result for each position
@@ -130,7 +144,7 @@ if __name__ == "__main__":
 #  
 #         filename = position + ".txt"
 #         for result in xml_results:
-#             append_to_text_file(filename, xml_result)
+#             pyfsi.clear_text_file_and_write(filename, xml_result)
                 
                 
     print("done")
