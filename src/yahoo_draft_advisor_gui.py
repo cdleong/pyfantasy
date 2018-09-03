5067b99351ebced01f10b4d210624b8fd228b352
cdleong <cdleong+github@gmail.com>
2018-09-03 15:01:39 -0400

    CDL: Stub GUI class

---
+++ src/yahoo_draft_advisor_gui.py
@@ -0,0 +1,18 @@
+'''
+UI for yahoo draft advisor
+Created on Sep 3, 2018
+
+@author: cdleong
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
